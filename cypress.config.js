2bdf3716e39e96cd4006c79b51f36d01e549a0ad
Gabriel Carneiro <carneirodesousagabriel@gmail.com>
2025-06-26 12:56:28 -0300

    adicionando o project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "r5jmut",
+  viewportWidth:1080,
+  viewportHeight:1920,
   e2e: {
     supportFile: false,
   },
